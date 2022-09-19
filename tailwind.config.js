8b159a512f110c0269592dbd4ab26e491fc226d5
sqirum <pauloslh94@gmail.com>
2022-09-19 23:14:24 +0100

    finish tailwind jit installation

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  mode: 'jit',
-  content: [],
+  mode:'jit',
+  purge: [
+    './**/*.html',
+    './src/**/*.{js,jsx,ts,tsx,vue}',
+  ],
+  content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
   },
-  plugins: [
-  ],
+  plugins: [],
 }
