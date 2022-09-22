1229d1d33b36002a2ecdd69d326610ccfc455890
sqirum <pauloslh94@gmail.com>
2022-09-22 23:46:22 +0100

    finish mobile html & css

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,15 @@ module.exports = {
   ],
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'bg-mobile': "url('/assets/bg-main-mobile.png')",
+        'bg-tablet': "url('/assets/bg-main-tablet.png')",
+        'bg-desktop': "url('/assets/bg-main-desktop.png')",
+        'bg-pattern-1': "url('/assets/bg-pattern-1.svg')",
+        'bg-pattern-2': "url('/assets/bg-pattern-2.svg')"
+      }
+    },
   },
   plugins: [],
 }
